4faf15b4eac8e39599d9bcf9f33108287cf7c15d
Jozef Reisinger <jozef.reisinger@gmail.com>
2023-10-16 14:14:25 +0200

    add Names type

--- cmd/recon/main.go
+++ cmd/recon/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/jreisinger/recon"
 	"github.com/jreisinger/recon/dns"
@@ -13,38 +14,45 @@ import (
 	"github.com/jreisinger/recon/tls"
 )
 
-var all = []recon.Reconnoiterer{
-	dns.Cname(),
-	dns.IPAddr(),
-	dns.MX(),
-	dns.NS(),
-	dns.TXT(),
-	geo.DBip(),
-	http.Version(),
-	tcp.OpenPorts(),
-	tls.CA(),
-	tls.Issuer(),
-	tls.Version(),
+var usage = func() {
+	fmt.Fprintf(flag.CommandLine.Output(), "%s - gather info about network hosts\n\n", os.Args[0])
+	fmt.Fprintf(flag.CommandLine.Output(), "options\n")
+	flag.PrintDefaults()
+}
+
+var names = Names{
+	"cname":   dns.Cname(),
+	"ips":     dns.IPAddr(),
+	"mx":      dns.MX(),
+	"ns":      dns.NS(),
+	"txt":     dns.TXT(),
+	"geo":     geo.DBip(),
+	"httpver": http.Version(),
+	"ports":   tcp.OpenPorts(),
+	"ca":      tls.CA(),
+	"iss":     tls.Issuer(),
+	"tlsver":  tls.Version(),
 }
 
 var (
 	c = flag.Int("c", 10, "max hosts being reconned concurrently")
 	j = flag.Bool("j", false, "json output")
-	r = flag.String("r", "", "run just this reconnoiterer")
+	r = flag.String(
+		"r",
+		"",
+		fmt.Sprintf(
+			"run just one of the reconnoiterers: %s",
+			strings.Join(names.GetAll(), " ")))
 )
 
 func main() {
-	flag.Usage = func() {
-		fmt.Fprintf(flag.CommandLine.Output(), "%s - gather info about network hosts\n\n", os.Args[0])
-		fmt.Fprintf(flag.CommandLine.Output(), "options\n")
-		flag.PrintDefaults()
-	}
+	flag.Usage = usage
 	flag.Parse()
 
 	var reconnoiterers []recon.Reconnoiterer
 
 	if *r == "" {
-		reconnoiterers = all
+		reconnoiterers = names.GetAllReconnoiterers()
 	} else {
 		switch *r {
 		case "cname":
@@ -70,7 +78,10 @@ func main() {
 		case "tlsver":
 			reconnoiterers = append(reconnoiterers, tls.Version())
 		default:
-			fmt.Fprintf(os.Stderr, "recon: unknown reconnoiterer: %s: pick one from: cname, ips, mx, ns, txt, geo, httpver, ports, ca, iss, tlsver\n", *r)
+			fmt.Fprintf(
+				os.Stderr,
+				"recon: unknown reconnoiterer: %s: pick one from: %s\n",
+				*r, strings.Join(names.GetAll(), " "))
 			os.Exit(1)
 		}
 	}
